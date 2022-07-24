4dc4052150163f1ceaf526435083e0595199325f
tutor6 <tutor6@hphk.kr>
2022-07-24 20:30:10 +0900

    Update 20220725

---
+++ 2회차/김현수/20220725/3_OX퀴즈.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/8958
+import sys
+
+sys.stdin = open("3_OX퀴즈.txt")
---
+++ 2회차/백솔비/20220725/1_상수.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/2908
+import sys
+
+sys.stdin = open("1_상수.txt")
