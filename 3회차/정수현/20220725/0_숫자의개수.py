41fdfd8dc253477ecd913728be3a14ab9355034b
wjdtngus9536 <wjdtngus9536@gmail.com>
2022-07-25 14:52:23 +0900

    [Add] 0725

--- 3회차/정수현/20220725/0_숫자의개수.py
+++ 3회차/정수현/20220725/0_숫자의개수.py
@@ -2,3 +2,12 @@
 import sys
 
 sys.stdin = open("0_숫자의개수.txt")
+data = [int(sys.stdin.readline()) for i in range(3)]
+data = str(data[0]*data[1]*data[2])
+table = {str(i):0 for i in range(10)}
+
+for i in data:
+    table[i] += 1
+
+for i in '0123456789':
+    print(table[i])
--- 3회차/정수현/20220725/2_두개뽑아서더하기.py
+++ 3회차/정수현/20220725/2_두개뽑아서더하기.py
@@ -1,7 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
-def solution(numbers):
-    answer = []
-    return answer
+def solution(numbers):            
+    answer = set()
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.add(numbers[i]+numbers[j])
+    return sorted(answer)
 
 
 print(solution([2, 1, 3, 4, 1]))
